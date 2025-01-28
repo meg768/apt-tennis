d33b1e1eca6007fa2ef4763673e102e00099f6b1
meg768 <magnus@egelberg.se>
2025-01-28 22:10:10 +0100

    -

--- src/probe.js
+++ src/probe.js
@@ -26,9 +26,14 @@ class Probe {
         let then = this.startTime;
         let now = this.stopTime;
         let seconds = Math.round(((now - then) / 1000) * 10) / 10;
+
+        if (seconds > 10) {
+            seconds = Math.round(seconds);
+        }
+
         let text = `${seconds} seconds`;
 
-        if (seconds > 60) {
+        if (seconds > 1) {
             text = `${Math.round(seconds / 60)} minute(s)`;
         }
 
